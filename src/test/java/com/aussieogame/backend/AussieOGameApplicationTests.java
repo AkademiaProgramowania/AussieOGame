8997c544f1db9e79181f882da7c7019d94dec2c4
Przemysław Frąckowiak-Szymański <7953131+dehydrogenaza@users.noreply.github.com>
2023-03-19 14:07:54 +0100

    Initial security config

--- src/test/java/com/aussieogame/backend/AussieOGameApplicationTests.java
+++ src/test/java/com/aussieogame/backend/AussieOGameApplicationTests.java
@@ -2,8 +2,10 @@ package com.aussieogame.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(profiles = "dev")
 class AussieOGameApplicationTests {
 
 	@Test
